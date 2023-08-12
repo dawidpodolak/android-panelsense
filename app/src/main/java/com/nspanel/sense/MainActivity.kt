2e9d1e9aeb0dc7ba06382fdb3296fa354239fc71
Dawid Podolak <dawidpod1@gmail.com>
2023-08-12 13:36:36 +0200

    Use kts

--- app/src/main/java/com/nspanel/sense/MainActivity.kt
+++ app/src/main/java/com/nspanel/sense/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.nspanel.sense
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,10 +20,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NSPanelSenseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                        Greeting("Android")
+                    }
             }
         }
     }
@@ -29,6 +31,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+    Text(text = "To jest test!")
 }
 
 @Preview(showBackground = true)
