cc6f0824c44e823d0aac0bfd329959ad060ecd73
Dawid Podolak <dawidpod1@gmail.com>
2023-12-05 16:30:17 +0100

    Bump AGP, kotlin and other libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.com.android.application) apply false
-    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.application) apply false
+    alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.dev.detekt)
     alias(libs.plugins.androidLibrary) apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
+    alias(libs.plugins.hiltLibrary) apply false
 }
 
 subprojects {
