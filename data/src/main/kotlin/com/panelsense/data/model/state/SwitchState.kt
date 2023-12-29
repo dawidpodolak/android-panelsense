59732932a9dd09e4717fb9a45159b273c8a9f64d
Dawid Podolak <dawidpod1@gmail.com>
2023-12-29 19:17:04 +0100

    [ISSUE-48] Refactor and fix incoming message processing

--- data/src/main/kotlin/com/panelsense/data/model/state/SwitchState.kt
+++ data/src/main/kotlin/com/panelsense/data/model/state/SwitchState.kt
@@ -1,8 +1,14 @@
 package com.panelsense.data.model.state
 
+import com.panelsense.data.mapper.toEntityState
+import com.panelsense.domain.model.entity.state.EntityState
+
 data class SwitchState(
     val entityId: String,
     val on: Boolean,
     val friendlyName: String?,
     val icon: String?
-)
+) : DataState {
+
+    override fun toDomainState(): EntityState = toEntityState()
+}
